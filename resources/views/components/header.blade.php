52931b3ae9633394ad4c6da2af7d34fe46651f6a
Wen-MY <ahwen2a7a9@gmail.com>
2024-04-13 21:57:27 +0800

    fix header file conflict and wrongly solved

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,8 +5,8 @@
 
     <title>Stock Management System</title>
 
-<!-- Bootstrap -->
-<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <!-- Bootstrap theme -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css') }}">
     <!-- Font Awesome -->
@@ -33,8 +33,7 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -51,80 +50,39 @@
 
                 <ul class="nav navbar-nav navbar-right">
 
-      	<li id="navDashboard"><a href="{{ url('/') }}"><i class="glyphicon glyphicon-list-alt"></i>  Dashboard</a></li>        
-        @if(session()->has('user') && session('user.id')==1)
-        <li id="navBrand"><a href="{{ url('/brand') }}"><i class="glyphicon glyphicon-btc"></i>  Brand</a></li>        
-		@endif
-		@if(session()->has('user') && session('user.id')==1)
-        <li id="navCategories"><a href="{{ url('/categories') }}"> <i class="glyphicon glyphicon-th-list"></i> Category</a></li>        
-		@endif
-		@if(session()->has('user') && session('user.id')==1)
-        <li id="navProduct"><a href="{{ url('/product') }}"> <i class="glyphicon glyphicon-ruble"></i> Product </a></li> 
-		@endif
-		
-        <li class="dropdown" id="navOrder">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-shopping-cart"></i> Orders <span class="caret"></span></a>
-          <ul class="dropdown-menu">            
-            <li id="topNavAddOrder"><a href="{{ url('/orders?o=add') }}"> <i class="glyphicon glyphicon-plus"></i> Add Orders</a></li>            
-            <li id="topNavManageOrder"><a href="{{ url('/orders?o=manord') }}"> <i class="glyphicon glyphicon-edit"></i> Manage Orders</a></li>            
-          </ul>
-        </li> 
-		
-		@if(session()->has('user') && session('user.id')==1)
-        <li id="navReport"><a href="{{ url('/report') }}"> <i class="glyphicon glyphicon-check"></i> Report </a></li>
-		@endif 
-    @if(session()->has('user') && session('user.id')==1)
-        <li id="importbrand"><a href="{{ url('/importbrand') }}"> <i class="glyphicon glyphicon-check"></i> Import Brand </a></li>
-		@endif   
-        <li class="dropdown" id="navSetting">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-user"></i> <span class="caret"></span></a>
-          <ul class="dropdown-menu">    
-			@if(session()->has('user') && session('user.id')==1)
-            <li id="topNavSetting"><a href="{{ url('/setting') }}"> <i class="glyphicon glyphicon-wrench"></i> Setting</a></li>
-            <li id="topNavUser"><a href="{{ url('/user') }}"> <i class="glyphicon glyphicon-wrench"></i> Add User</a></li>
-@endif              
-            <li id="topNavLogout"><a href="{{ url('/logout') }}"> <i class="glyphicon glyphicon-log-out"></i> Logout</a></li>            
-          </ul>
-        </li>        
-           
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-	</nav>
+                    <li id="navDashboard"><a href="{{ url('/') }}"><i class="glyphicon glyphicon-list-alt"></i> Dashboard</a></li>
+                    @if(session()->has('user') && session('user.id')==1)
+                    <li id="navBrand"><a href="{{ url('/brand') }}"><i class="glyphicon glyphicon-btc"></i> Brand</a></li>
+                    @endif
+                    @if(session()->has('user') && session('user.id')==1)
+                    <li id="navCategories"><a href="{{ url('/categories') }}"> <i class="glyphicon glyphicon-th-list"></i> Category</a></li>
+                    @endif
+                    @if(session()->has('user') && session('user.id')==1)
+                    <li id="navProduct"><a href="{{ url('/product') }}"> <i class="glyphicon glyphicon-ruble"></i> Product </a></li>
+                    @endif
 
                     <li class="dropdown" id="navOrder">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false"> <i
-                                class="glyphicon glyphicon-shopping-cart"></i> Orders <span class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-shopping-cart"></i> Orders <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            <li id="topNavAddOrder"><a href="{{ url('/orders?o=add') }}"> <i
-                                        class="glyphicon glyphicon-plus"></i> Add Orders</a></li>
-                            <li id="topNavManageOrder"><a href="{{ url('/orders?o=manord') }}"> <i
-                                        class="glyphicon glyphicon-edit"></i> Manage Orders</a></li>
+                            <li id="topNavAddOrder"><a href="{{ url('/orders?o=add') }}"> <i class="glyphicon glyphicon-plus"></i> Add Orders</a></li>
+                            <li id="topNavManageOrder"><a href="{{ url('/orders?o=manord') }}"> <i class="glyphicon glyphicon-edit"></i> Manage Orders</a></li>
                         </ul>
                     </li>
 
-                    @if (session()->has('user') && session('user.id') == 1)
-                        <li id="navReport"><a href="{{ url('/report') }}"> <i class="glyphicon glyphicon-check"></i>
-                                Report </a></li>
+                    @if(session()->has('user') && session('user.id')==1)
+                    <li id="navReport"><a href="{{ url('/report') }}"> <i class="glyphicon glyphicon-check"></i> Report </a></li>
                     @endif
-                    @if (session()->has('user') && session('user.id') == 1)
-                        <li id="importbrand"><a href="{{ url('/importbrand') }}"> <i
-                                    class="glyphicon glyphicon-check"></i> Import Brand </a></li>
+                    @if(session()->has('user') && session('user.id')==1)
+                    <li id="importbrand"><a href="{{ url('/importbrand') }}"> <i class="glyphicon glyphicon-check"></i> Import Brand </a></li>
                     @endif
                     <li class="dropdown" id="navSetting">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-user"></i> <span
-                                class="caret"></span></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="glyphicon glyphicon-user"></i> <span class="caret"></span></a>
                         <ul class="dropdown-menu">
-                            @if (session()->has('user') && session('user.id') == 1)
-                                <li id="topNavSetting"><a href="{{ url('/setting') }}"> <i
-                                            class="glyphicon glyphicon-wrench"></i> Setting</a></li>
-                                <li id="topNavUser"><a href="{{ url('/user') }}"> <i
-                                            class="glyphicon glyphicon-wrench"></i> Add User</a></li>
+                            @if(session()->has('user') && session('user.id')==1)
+                            <li id="topNavSetting"><a href="{{ url('/setting') }}"> <i class="glyphicon glyphicon-wrench"></i> Setting</a></li>
+                            <li id="topNavUser"><a href="{{ url('/user') }}"> <i class="glyphicon glyphicon-wrench"></i> Add User</a></li>
                             @endif
-                            <li id="topNavLogout"><a href="{{ url('/logout') }}"> <i
-                                        class="glyphicon glyphicon-log-out"></i> Logout</a></li>
+                            <li id="topNavLogout"><a href="{{ url('/logout') }}"> <i class="glyphicon glyphicon-log-out"></i> Logout</a></li>
                         </ul>
                     </li>
 
@@ -132,5 +90,4 @@
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
-
     <div class="container">
